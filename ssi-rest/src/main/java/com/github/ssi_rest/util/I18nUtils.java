b82f82d48ce0977f7c882d03646bac4e281314cc
Eric R Glass <ericrglass@gmail.com>
2013-07-27 13:06:16 -0400

    Updated to utilize the new ssi-servlet utility JAR.

--- ssi-rest/src/main/java/com/github/ssi_rest/util/I18nUtils.java
+++ ssi-rest/src/main/java/com/github/ssi_rest/util/I18nUtils.java
@@ -1,7 +1,5 @@
 package com.github.ssi_rest.util;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -9,27 +7,24 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Properties;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 
 import org.codehaus.jackson.map.ObjectMapper;
 
 import com.github.ssi_rest.SsiRestLogger;
+import com.github.ssi_servlet.SsiServletUtils;
 
-public class I18nUtils {
+public class I18nUtils extends SsiServletUtils {
 
 	public static final String I18N_JSON_PROPERTY = "i18n";
 	public static final String I18N_JSON_KEY_LANG = "lang";
 	public static final String I18N_JSON_KEY_DIR = "dir";
 	public static final String I18N_JSON_EMPTY_MAP = "{\"" + I18N_JSON_PROPERTY
 			+ "\":{}}";
-	public static final Locale DEFAULT_LOCALE = Locale.US;
-	public static final String DEFAULT_HTML_LANG = DEFAULT_LOCALE.toString()
-			.replaceFirst("_", "-");
 
 	public static final List<String> CURRENT_SUPPORTED_LANGUAGES = new ArrayList<String>();
 	public static final Map<String, List<String>> CURRENT_SUPPORTED_LANGUAGE_COUNTRIES = new HashMap<String, List<String>>();
-	public static final List<String> RTL_LANGUAGES = new ArrayList<String>();
 
 	static {
 		CURRENT_SUPPORTED_LANGUAGES.add("ar");
@@ -40,6 +35,7 @@ public class I18nUtils {
 		CURRENT_SUPPORTED_LANGUAGES.add("he");
 		CURRENT_SUPPORTED_LANGUAGES.add("hi");
 		CURRENT_SUPPORTED_LANGUAGES.add("it");
+		CURRENT_SUPPORTED_LANGUAGES.add("iw");
 		CURRENT_SUPPORTED_LANGUAGES.add("ja");
 		CURRENT_SUPPORTED_LANGUAGES.add("ko");
 		CURRENT_SUPPORTED_LANGUAGES.add("zh");
@@ -181,6 +177,7 @@ public class I18nUtils {
 		// Hebrew - Israel
 		heCountries.add("IL");
 		CURRENT_SUPPORTED_LANGUAGE_COUNTRIES.put("he", heCountries);
+		CURRENT_SUPPORTED_LANGUAGE_COUNTRIES.put("iw", heCountries);
 
 		List<String> hiCountries = new ArrayList<String>();
 		// Hindi - India
@@ -216,16 +213,6 @@ public class I18nUtils {
 		// Chinese - Taiwan
 		zhCountries.add("TW");
 		CURRENT_SUPPORTED_LANGUAGE_COUNTRIES.put("zh", zhCountries);
-
-		RTL_LANGUAGES.add("ar");
-		RTL_LANGUAGES.add("dv");
-		RTL_LANGUAGES.add("fa");
-		RTL_LANGUAGES.add("he");
-		RTL_LANGUAGES.add("prs");
-		RTL_LANGUAGES.add("ps");
-		RTL_LANGUAGES.add("syr");
-		RTL_LANGUAGES.add("ug");
-		RTL_LANGUAGES.add("ur");
 	}
 
 	public static Map<String, String> getCurrentSupportedLanguagesMap(
@@ -336,67 +323,6 @@ public class I18nUtils {
 		return curSupLangCountriesMap;
 	}
 
-	public static boolean isRightToLeftLanguage(String language) {
-		if ((language == null) || (language.trim().length() == 0)) {
-			return false;
-		}
-
-		return RTL_LANGUAGES.contains(language.trim());
-	}
-
-	public static boolean isRightToLeftLanguage(Locale locale) {
-		if (locale == null) {
-			return false;
-		}
-
-		return isRightToLeftLanguage(locale.getLanguage());
-	}
-
-	public static String getLanguageHTMLDirAttributeValue(String language) {
-		if ((language == null) || (language.trim().length() == 0)) {
-			return "ltr";
-		}
-
-		if (isRightToLeftLanguage(language.trim())) {
-			return "rtl";
-		}
-
-		return "ltr";
-	}
-
-	public static String getLanguageHTMLDirAttributeValue(Locale locale) {
-		if (locale == null) {
-			return "ltr";
-		}
-
-		return getLanguageHTMLDirAttributeValue(locale.getLanguage());
-	}
-
-	public static String getCultureHTMLLangAttributeValue(String language,
-			String country) {
-		if ((language == null) || (language.trim().length() == 0)) {
-			return DEFAULT_HTML_LANG;
-		}
-
-		StringBuilder htmlLang = new StringBuilder(language.trim());
-
-		if ((country != null) && (country.trim().length() > 0)) {
-			htmlLang.append("-");
-			htmlLang.append(country.trim());
-		}
-
-		return htmlLang.toString();
-	}
-
-	public static String getCultureHTMLLangAttributeValue(Locale locale) {
-		if (locale == null) {
-			return DEFAULT_HTML_LANG;
-		}
-
-		return getCultureHTMLLangAttributeValue(locale.getLanguage(),
-				locale.getCountry());
-	}
-
 	public static String getI18nJSONFromResourceBundleMap(
 			Map<String, String> resBundleMap, String language, String country,
 			boolean debug) {
@@ -436,7 +362,7 @@ public class I18nUtils {
 					+ json);
 		}
 
-		return json;
+		return json.replaceAll("\\\\u", "\\u");
 	}
 
 	public static String getI18nJSONFromResourceBundleMap(
@@ -452,168 +378,103 @@ public class I18nUtils {
 
 	public static Map<String, String> getCultureResourceBundleMap(
 			String resBaseName, String language, String country,
-			String resPackage, String resNameSeparator, String resNameSuffix,
-			boolean debug) {
+			String resPackage, boolean javaClass, boolean debug) {
 		if ((resBaseName == null) || (resBaseName.trim().length() == 0)
 				|| (language == null) || (language.trim().length() == 0)) {
 			return new HashMap<String, String>();
 		}
 
 		StringBuilder resBasePath = new StringBuilder();
-		String baseName = resBaseName.trim();
 
 		if ((resPackage != null) && (resPackage.trim().length() > 0)) {
-			resBasePath.append(resPackage.trim().replaceAll("\\.", "/"));
-			String path = resBasePath.toString();
+			resBasePath.append(resPackage.trim());
 
-			if ((!path.endsWith("/") || !path.endsWith("\\"))
-					&& (!baseName.startsWith("/") || !baseName.startsWith("\\"))) {
-				resBasePath.append("/");
+			if (!resBaseName.trim().startsWith(".")
+					&& !resPackage.trim().endsWith(".")) {
+				resBasePath.append(".");
 			}
 		}
 
-		resBasePath.append(baseName);
-		String resNameSep = resNameSeparator;
+		resBasePath.append(resBaseName.trim());
+		Locale culture = null;
 
-		if ((resNameSep == null) || (resNameSep.trim().length() == 0)) {
-			// The default for the resource name separator: _
-			resNameSep = "_";
+		if ((country != null) && (country.trim().length() > 0)) {
+			culture = new Locale(language.trim(), country.trim());
+
+			if (debug) {
+				SsiRestLogger.LOGGER
+						.warning("Debug Message: I18nUtils - "
+								+ "getCultureResourceBundleMap - is using the resource base '"
+								+ resBasePath + "' with the language '"
+								+ language.trim() + "' and the country '"
+								+ country.trim() + "'.");
+			}
+		} else {
+			culture = new Locale(language.trim());
+
+			if (debug) {
+				SsiRestLogger.LOGGER
+						.warning("Debug Message: I18nUtils - "
+								+ "getCultureResourceBundleMap - is using the resource base '"
+								+ resBasePath + "' with the language '"
+								+ language.trim() + "'.");
+			}
 		}
 
-		String resNameSuf = resNameSuffix;
+		ResourceBundle bundle = null;
 
-		if ((resNameSuf == null) || (resNameSuf.trim().length() == 0)) {
-			// The default for the resource name suffix: .properties
-			resNameSuf = ".properties";
+		try {
+			if (javaClass) {
+				bundle = I18nResourceBundle.getI18nClassBundle(
+						resBasePath.toString(), culture);
+			} else {
+				bundle = I18nResourceBundle.getI18nPropertiesBundle(
+						resBasePath.toString(), culture);
+			}
+		} catch (Exception e) {
+			bundle = null;
 		}
 
 		if (debug) {
 			SsiRestLogger.LOGGER
 					.warning("Debug Message: I18nUtils - "
-							+ "getCultureResourceBundleMap - is using the resource base name '"
-							+ baseName + "' with the language '"
-							+ language.trim() + "'.");
+							+ "getCultureResourceBundleMap - the resource bundle "
+							+ ((bundle == null) ? "could not be found."
+									: "was found and is for locale '"
+											+ (((bundle.getLocale() != null) && (bundle
+													.getLocale().toString()
+													.length() > 0)) ? bundle
+													.getLocale().toString()
+													: "DEFAULT: "
+															+ DEFAULT_LOCALE
+																	.toString())
+											+ "'."));
 		}
 
-		StringBuilder languageResPath = new StringBuilder(resBasePath);
-		languageResPath.append(resNameSep);
-		languageResPath.append(language.trim());
-		languageResPath.append(resNameSuf);
+		Map<String, String> resMap = null;
 
-		Map<String, String> resMap = new HashMap<String, String>();
+		if (bundle instanceof I18nPropertyResourceBundle) {
+			resMap = ((I18nPropertyResourceBundle) bundle).getMap();
+		} else if (bundle != null) {
+			resMap = new HashMap<String, String>();
 
-		Properties languageProps = new Properties();
-		InputStream languageIS = Thread.currentThread().getContextClassLoader()
-				.getResourceAsStream(languageResPath.toString());
-
-		try {
-			if (languageIS != null) {
-				languageProps.load(languageIS);
-				languageIS.close();
-				languageIS = null;
-			} else {
-				languageProps = null;
-			}
-		} catch (IOException e) {
-			languageProps = null;
-		} finally {
-			if (languageIS != null) {
-				try {
-					languageIS.close();
-				} catch (IOException e) {
-				}
-
-				languageIS = null;
-			}
-		}
-
-		if (languageProps != null) {
-			for (Object key : languageProps.keySet()) {
+			for (String key : bundle.keySet()) {
 				if (key == null) {
 					continue;
 				}
 
-				Object value = languageProps.get(key);
+				String value = bundle.getString(key);
 
-				if ((value != null) && (value.toString().length() > 0)) {
-					resMap.put(key.toString(), value.toString());
+				if (value == null) {
+					continue;
 				}
+
+				resMap.put(key, value);
 			}
 		}
 
-		if (debug) {
-			SsiRestLogger.LOGGER.warning("Debug Message: I18nUtils - "
-					+ "getCultureResourceBundleMap - the language "
-					+ "resource bundle '"
-					+ languageResPath
-					+ ((languageProps == null) ? "' could not be found."
-							: "' was found and used."));
-		}
-
-		StringBuilder cultureResPath = new StringBuilder();
-
-		if ((country != null) && (country.trim().length() > 0)) {
-			cultureResPath.append(resBasePath);
-			cultureResPath.append(resNameSep);
-			cultureResPath.append(language.trim());
-			cultureResPath.append(resNameSep);
-			cultureResPath.append(country.trim());
-			cultureResPath.append(resNameSuf);
-		}
-
-		if (cultureResPath.length() > 0) {
-			Properties cultureProps = new Properties();
-			InputStream cultureIS = Thread.currentThread()
-					.getContextClassLoader()
-					.getResourceAsStream(cultureResPath.toString());
-
-			try {
-				if (cultureIS != null) {
-					cultureProps.load(cultureIS);
-					cultureIS.close();
-					cultureIS = null;
-				} else {
-					cultureProps = null;
-				}
-			} catch (IOException e) {
-				cultureProps = null;
-			} finally {
-				if (cultureIS != null) {
-					try {
-						cultureIS.close();
-					} catch (IOException e) {
-					}
-
-					cultureIS = null;
-				}
-			}
-
-			if (cultureProps != null) {
-				for (Object key : cultureProps.keySet()) {
-					if (key == null) {
-						continue;
-					}
-
-					Object value = cultureProps.get(key);
-
-					if ((value != null) && (value.toString().length() > 0)) {
-						resMap.put(key.toString(), value.toString());
-					}
-				}
-			}
-
-			if (debug) {
-				SsiRestLogger.LOGGER.warning("Debug Message: I18nUtils - "
-						+ "getCultureResourceBundleMap - the culture "
-						+ "resource bundle '"
-						+ cultureResPath
-						+ ((cultureProps == null) ? "' could not be found."
-								: "' was found and used."));
-			}
-		} else if (debug) {
-			SsiRestLogger.LOGGER.warning("Debug Message: I18nUtils - "
-					+ "getCultureResourceBundleMap - the country was not "
-					+ "provided, so there is no culture resource bundle.");
+		if (resMap == null) {
+			resMap = new HashMap<String, String>();
 		}
 
 		if (debug) {
@@ -626,20 +487,19 @@ public class I18nUtils {
 
 	public static Map<String, String> getCultureResourceBundleMap(
 			String resBaseName, Locale locale, String resPackage,
-			String resNameSeparator, String resNameSuffix, boolean debug) {
+			boolean javaClass, boolean debug) {
 		if ((resBaseName == null) || (resBaseName.trim().length() == 0)
 				|| (locale == null)) {
 			return new HashMap<String, String>();
 		}
 
 		return getCultureResourceBundleMap(resBaseName, locale.getLanguage(),
-				locale.getCountry(), resPackage, resNameSeparator,
-				resNameSuffix, debug);
+				locale.getCountry(), resPackage, javaClass, debug);
 	}
 
 	public static String getCultureResourceBundleJSON(String resBaseName,
 			String language, String country, String resPackage,
-			String resNameSeparator, String resNameSuffix, boolean debug) {
+			boolean javaClass, boolean debug) {
 		if ((resBaseName == null) || (resBaseName.trim().length() == 0)
 				|| (language == null) || (language.trim().length() == 0)) {
 			return I18N_JSON_EMPTY_MAP;
@@ -647,21 +507,18 @@ public class I18nUtils {
 
 		return getI18nJSONFromResourceBundleMap(
 				getCultureResourceBundleMap(resBaseName, language, country,
-						resPackage, resNameSeparator, resNameSuffix, debug),
-				language, country, debug);
+						resPackage, javaClass, debug), language, country, debug);
 	}
 
 	public static String getCultureResourceBundleJSON(String resBaseName,
-			Locale locale, String resPackage, String resNameSeparator,
-			String resNameSuffix, boolean debug) {
+			Locale locale, String resPackage, boolean javaClass, boolean debug) {
 		if ((resBaseName == null) || (resBaseName.trim().length() == 0)
 				|| (locale == null)) {
 			return I18N_JSON_EMPTY_MAP;
 		}
 
 		return getCultureResourceBundleJSON(resBaseName, locale.getLanguage(),
-				locale.getCountry(), resPackage, resNameSeparator,
-				resNameSuffix, debug);
+				locale.getCountry(), resPackage, javaClass, debug);
 	}
 
 	public static class I18nJSONBean {
